cdbe6c892934577d37c9cb4ee35e819b7d60db17
Muhammadali0981 <muhammadali030605@gmail.com>
2024-03-21 21:43:05 +0500

    de

---
+++ parctice.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class comment{
+
+}
+class Post
+{
+private:
+    string title,content;
+    Comment* comments;
+
+public:
+    
+};
+
+
+
+int main()
+{
+    
+    return 0;
+}
